accc5dbd61f71be4714544a86c0064f8bf2291f6
User <User@DESKTOP-R9E659M>
2024-01-28 16:43:26 +0530

    First commit

---
+++ EclipseGitIntegration/src/main/java/com/githubIntegration/EclipseGitIntegration/App.java
@@ -0,0 +1,13 @@
+package com.githubIntegration.EclipseGitIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
